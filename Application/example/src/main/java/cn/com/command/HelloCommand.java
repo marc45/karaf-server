5b7972168967c30bd1b7484abddee889c39c780f
xiaxuan <xiaxuan@haizhi.com>
2016-04-20 19:12:27 +0800

    增加一个command

--- Application/example/src/main/java/cn/com/command/HelloCommand.java
+++ Application/example/src/main/java/cn/com/command/HelloCommand.java
@@ -1,7 +1,21 @@
 package cn.com.command;
 
+import lombok.extern.log4j.Log4j;
+import org.apache.felix.gogo.commands.Action;
+import org.apache.felix.service.command.CommandSession;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+
 /**
  * Created by xiaxuan on 16/4/20.
  */
-public class HelloCommand {
+@Service
+@Log4j
+@Command(scope = "test", name = "hello", description = "start for hello world")
+public class HelloCommand implements Action{
+
+    public Object execute(CommandSession session) throws Exception {
+        log.info("Hello word");
+        return null;
+    }
 }
